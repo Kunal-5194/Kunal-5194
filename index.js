e494fddfcb97984a4a07f2c23de2d2c8414ed2dc
Kunal-5194 <131642479+Kunal-5194@users.noreply.github.com>
2023-07-07 13:55:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// let a =3;
+// let b =4;
+// let c = a+b;
+// console.log(c);
+// let example = require("./example");
+// console.log(example.y);
+// console.log(example.b);
+const arr = [1,3,4,5,63,8,9];
+let result = arr.filter((item)=>{
+    return(item<8);
+})
+console.log(result);
